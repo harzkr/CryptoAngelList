24399694ce90e0d6533ee5b179bd77b6232e17c0
Harsha <harzkr142@gmail.com>
2020-11-20 08:04:46 +0530

    Updates

---
+++ webpack.production.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+    context: __dirname,
+    entry: './frontend/crypto_list.jsx',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            },      
+            {
+                test: /\.(png|svg|jpg|gif|webp)$/,
+                loader: "file-loader",
+                options: { name: '[path][name].[ext]' }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
+    },
+    plugins: [    
+        new webpack.DefinePlugin({           
+          COINMARKETCAP_API_KEY: JSON.stringify(process.env.COINMARKETCAP_API_KEY)
+        })
+      ],      
+};
