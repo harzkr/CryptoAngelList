b006a382af972c3681646178354d78fc40c82782
Harsha <harzkr142@gmail.com>
2020-11-20 22:09:19 +0530

    Updates

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module CryptoList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.autoloader = :classic
+    #config.autoloader = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
